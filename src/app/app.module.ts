afd9162cd95398634f8c3a4887c4a963fc25b6a9
AidaJesusSora <ajsora97@gmail.com>
2021-01-19 16:31:52 +0100

    version-0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Componente1Component } from './componente1/componente1.component';
-import { Componente2Component } from './componente2/componente2.component';
+import { Componente1Component } from './components/componente1/componente1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Componente1Component,
-    Componente2Component
   ],
   imports: [
     BrowserModule
